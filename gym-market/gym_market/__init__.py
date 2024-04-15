db03a5ed42fdfa08f1b9abf82c7c3d834a1e5ad0
YiquanTAT <wr23959@bristol.ac.uk>
2024-04-15 17:13:45 +0100

    Add random initialize env

--- gym-market/gym_market/__init__.py
+++ gym-market/gym_market/__init__.py
@@ -6,5 +6,4 @@ logger = logging.getLogger(__name__)
 register(
     id='Market-v0',
     entry_point='gym_market.envs:Market',
-
 )
--- gym-market/gym_market/envs/gym_market.py
+++ gym-market/gym_market/envs/gym_market.py
@@ -1,6 +1,10 @@
 import gymnasium as gym
 from gymnasium import spaces
+import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import os
+import random
 import logging
 
 MAX_ACCOUNT_BALANCE = 2147483647
@@ -17,32 +21,69 @@ logger = logging.getLogger(__name__)
 
 
 class Market(gym.Env):
-    metadata = {'render.modes': ['human']}
+    metadata = {'render_modes': ['human']}
 
     def _get_info(self):
         return {}
 
-    def __init__(self, lob_data, tape_data):
+    def _ramdom_data(self,lobs_data_dir,tapes_data_dir):
+        data_list = list(zip(os.listdir(lobs_data_dir), os.listdir(tapes_data_dir)))
+        random_index = random.randint(0, len(data_list) - 1)
+        lob_data_dir = os.path.join(lobs_data_dir, data_list[random_index][0])
+        tape_data_dir = os.path.join(tapes_data_dir, data_list[random_index][1])
+        lob_data = pd.read_csv(lob_data_dir).dropna()
+        self.tape_data = pd.read_csv(tape_data_dir)
+
+        selected_features = ['start_time', 'log_return1_realized_volatility', 'log_return1_realized_volatility.1',
+                             'log_return1_realized_volatility.2',
+                             'log_return1_realized_volatility.3',
+                             'log_return1_realized_volatility.4',
+                             'log_return1_realized_volatility.5', 'total_volume_sum', 'total_volume_sum.1',
+                             'total_volume_sum.2',
+                             'total_volume_sum.3', 'total_volume_sum.4', 'total_volume_sum.5',
+                             'transaction_quantity_sum', 'transaction_quantity_sum.1',
+                             'transaction_quantity_sum.2', 'transaction_quantity_sum.3',
+                             'transaction_quantity_sum.4', 'transaction_quantity_sum.5', 'transaction_count_sum',
+                             'transaction_count_sum.1',
+                             'transaction_count_sum.2', 'transaction_count_sum.3',
+                             'transaction_count_sum.4', 'transaction_count_sum.5', 'price_spread_sum',
+                             'price_spread_sum.1', 'price_spread_sum.2',
+                             'price_spread_sum.3', 'price_spread_sum.4', 'price_spread_sum.5', 'bid_spread_sum',
+                             'bid_spread_sum.1', 'bid_spread_sum.2',
+                             'bid_spread_sum.3', 'bid_spread_sum.4', 'bid_spread_sum.5', 'ask_spread_sum',
+                             'ask_spread_sum.1', 'ask_spread_sum.2',
+                             'ask_spread_sum.3', 'ask_spread_sum.4', 'ask_spread_sum.5', 'volume_imbalance_sum',
+                             'volume_imbalance_sum.1',
+                             'volume_imbalance_sum.2', 'volume_imbalance_sum.3',
+                             'volume_imbalance_sum.4', 'volume_imbalance_sum.5', 'bid_ask_spread_sum',
+                             'bid_ask_spread_sum.1', 'bid_ask_spread_sum.2',
+                             'bid_ask_spread_sum.3', 'bid_ask_spread_sum.4', 'bid_ask_spread_sum.5', 'size_tau2',
+                             'size_tau2.1', 'size_tau2.2', 'size_tau2.3', 'size_tau2.4',
+                             'size_tau2.5']
+
+        lob_data = lob_data[selected_features]
+        scaler = MinMaxScaler()
+        normalized_data = scaler.fit_transform(lob_data.values)
+        self.lob_data = pd.DataFrame(normalized_data, columns=lob_data.columns)
+
+    def __init__(self, lob_data_dir, tape_data_dir, render_mode='human'):
         super(Market, self).__init__()
-        self.lob_data = lob_data
-        self.tape_data = tape_data
+        self.lob_data_dir = lob_data_dir
+        self.tape_data_dir = tape_data_dir
+        self._ramdom_data(self.lob_data_dir,self.tape_data_dir)
+        self.render_mode = render_mode
         self.reward_range = (0, MAX_ACCOUNT_BALANCE)
         # 0:buy,1:sell,2:hold
         # (0 - 1) * 10 seconds
-        # self.action_space = spaces.Tuple((spaces.Box(low=0, high=3, shape=(1,), dtype=np.float32),
-        #                                  spaces.Box(low=0, high=3, shape=(1,), dtype=np.float32),
-        #                                  spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32),
-        #                                  spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)))
-        # self.action_space = spaces.Tuple((spaces.Box(low=-1.5, high=1.5, shape=(1,), dtype=np.float64),
-        #                                   spaces.Box(low=-1.5, high=1.5, shape=(1,), dtype=np.float64),
-        #                                   spaces.Box(low=-0.5, high=0.5, shape=(1,), dtype=np.float64),
-        #                                   spaces.Box(low=-0.5, high=0.5, shape=(1,), dtype=np.float64)))
-        self.action_space = spaces.Box(low=-1, high=1, shape=(4,), dtype=np.float64)
-        self.observation_space = spaces.Box(low=-1, high=1, shape=(201,), dtype=np.float64)
-        self.action_history = np.zeros(12)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(6,), dtype=np.float64)
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(207,), dtype=np.float64)
+        self.action_history = np.zeros(18)
+        self.trade_data = []
+        self.last_balance = INITIAL_ACCOUNT_BALANCE
+        # self.sell_reward = [0, 0]
 
     def _next_observation(self):
-        zeros_array = np.zeros(183)  # assume there are 61 features in lob_data
+        zeros_array = np.zeros(183)  # there are 61 features in lob_data
         frame = np.array(self.lob_data.iloc[self.current_step])
 
         if self.current_step == 0:
@@ -63,20 +104,25 @@ class Market(gym.Env):
             self.total_shares_sold / MAX_NUM_SHARES,
             self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
         ])
-        obs = np.append(obs, self.action_history)  # append length = 12
+        obs = np.append(obs, self.action_history)  # append length = 18
         return obs
 
-    def _take_one_action(self, action):
+    def _take_one_action(self, action, action_number):
         action_true_movement_time = self.current_step * 10 + ((action[1] + 1) / 2) * 10
         self.tape_data['time_differ'] = abs(self.tape_data['Time'] - action_true_movement_time)
         nearest_index = self.tape_data['time_differ'].idxmin()
         current_price = self.tape_data.loc[nearest_index]['Weighted_Price']
 
         action_type = (action[0] + 1) * 1.5
-        amount = 1
+        amount = int((action[2] + 1) * 5)
+        flag = 0
 
-        if action_type < 1:
+        # if action_number == 0:
+        #     self.sell_reward = []
+
+        if action_type <= 1 and self.balance >= amount * current_price:
             # buy
+            flag = 1
             shares_bought = amount
             prev_cost = self.cost_basis * self.shares_held
             additional_cost = shares_bought * current_price
@@ -88,13 +134,30 @@ class Market(gym.Env):
                 self.cost_basis = 0
             self.shares_held += shares_bought
 
-        elif action_type < 2:
+        elif action_type <= 2 and self.shares_held > amount:
             # sell
+            flag = 1
             shares_sold = amount
             self.balance += shares_sold * current_price
             self.shares_held -= shares_sold
             self.total_shares_sold += shares_sold
             self.total_sales_value += shares_sold * current_price
+
+            # sell_reward = shares_sold * (current_price - self.cost_basis)
+            # self.sell_reward[action_number] = sell_reward
+
+        elif action_type <= 3:
+            # hold
+            flag = 1
+        #     current_step_end_time = (self.current_step + 1) * 10
+        #     self.tape_data['time_differ'] = abs(self.tape_data['Time'] - current_step_end_time)
+        #     nearest_index = self.tape_data['time_differ'].idxmin()
+        #     current_price = self.tape_data.loc[nearest_index]['Weighted_Price']
+
+        self.balances_of_each_action[action_number] = self.balance
+        trade_info = [int(action_type), amount, current_price, flag]
+
+        self.trade_data.append(trade_info)
 
         self.net_worth = self.balance + self.shares_held * current_price
 
@@ -105,10 +168,15 @@ class Market(gym.Env):
             self.cost_basis = 0
 
     def _take_action(self, actions):
-        self._take_one_action(np.array([actions[0], actions[2]]))
-        self._take_one_action(np.array([actions[1], actions[3]]))
+        self.balances_of_each_action = [0, 0]
+        if actions[2] < actions[3]:
+            self._take_one_action(np.array([actions[0], actions[2], actions[4]]), 0)
+            self._take_one_action(np.array([actions[1], actions[3], actions[5]]), 1)
+        else:
+            self._take_one_action(np.array([actions[1], actions[3], actions[5]]), 0)
+            self._take_one_action(np.array([actions[0], actions[2], actions[4]]), 1)
         self.action_history = np.append(self.action_history, actions)
-        self.action_history = np.delete(self.action_history, [0, 1, 2, 3])
+        self.action_history = np.delete(self.action_history, [0, 1, 2, 3, 4, 5])
 
     def step(self, action):
         self._take_action(action)
@@ -118,17 +186,30 @@ class Market(gym.Env):
         if self.current_step >= len(self.lob_data):
             self.current_step = 0
 
+        # delay_modifier = (self.current_step / MAX_STEPS)
+        # reward = self.balance * delay_modifier
+
+        # reward = (self.balance - INITIAL_ACCOUNT_BALANCE / INITIAL_ACCOUNT_BALANCE) * (self.current_step / MAX_STEPS)
+
+        # reward = self.balance - self.last_balance
+        # self.last_balance = self.balance
+
+        # reward = (self.balances_of_each_action[1] - self.balances_of_each_action[0]) + (self.balance - self.last_balance)
+        # self.last_balance = self.balance
+
+        # reward = self.balance/INITIAL_ACCOUNT_BALANCE
         delay_modifier = (self.current_step / MAX_STEPS)
+        reward = (self.balance * delay_modifier + (self.balances_of_each_action[1] - self.balances_of_each_action[0]) + (self.balance - self.last_balance))/INITIAL_ACCOUNT_BALANCE
 
-        reward = self.balance * delay_modifier
+        self.reward = reward
 
-        terminated = self.current_step >= len(self.lob_data) - 1
+        terminated = self.current_step >= MAX_STEPS
 
         truncated = bool(self.net_worth <= 0)
 
         obs = self._next_observation()
 
-        return obs, reward, terminated ,truncated,self._get_info()
+        return obs, reward, terminated, truncated, self._get_info()
 
     def reset(self, seed=None, options=None):
         self.balance = INITIAL_ACCOUNT_BALANCE
@@ -141,18 +222,26 @@ class Market(gym.Env):
 
         self.current_step = 0
 
-        return self._next_observation(),self._get_info()
+        self.trade_data = []
 
-    def render(self, mode='human', close=False):
+        self.last_balance = INITIAL_ACCOUNT_BALANCE
+        self._ramdom_data(self.lob_data_dir, self.tape_data_dir)
+        return self._next_observation(), self._get_info()
+
+    def render(self, close=False):
         # Render the environment to the screen
-        profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
+        if self.render_mode == 'human':
+            profit = self.net_worth - INITIAL_ACCOUNT_BALANCE
 
-        print(f'Step: {self.current_step}')
-        print(f'Balance: {self.balance}')
-        print(
-            f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
-        print(
-            f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
-        print(
-            f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
-        print(f'Profit: {profit}')
+            print(f'Step: {self.current_step}')
+            print(f'Balance: {self.balance}')
+            print(f'Action0:{self.trade_data[self.current_step - 1]}')
+            print(f'Action1:{self.trade_data[self.current_step]}')
+            print(f'Reward: {self.reward}')
+            print(
+                f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
+            print(
+                f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
+            print(
+                f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
+            print(f'Profit: {profit}')
